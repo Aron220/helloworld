7eac18ef034afd92fea42f2e58d73de826811f35
Aron220 <109858751+Aron220@users.noreply.github.com>
2023-05-15 11:43:45 +0200

    Introductie
    Dit is de eerste commit van het project

---
+++ Intro.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("Ik heet Aron");
+        System.out.println("Ik ben 23 jaar");
+        System.out.println("Ik kom uit Groningen");
+    }
+}
