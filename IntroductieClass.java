b7ec4f7fd710e73bef13eea370f8722061639ebc
Aron220 <109858751+Aron220@users.noreply.github.com>
2023-05-15 13:16:36 +0200

    nieuwe class in nieuwe file gemaakt
    zie titel

---
+++ IntroductieClass.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+import java.sql.SQLOutput;
+
+public class IntroductieClass {
+    public String naam;
+    public IntroductieClass(String naam) {
+        System.out.println("Hallo");
+        System.out.println(naam);
+        System.out.println("!");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Ik heet Aron");
+//        System.out.println("Ik ben 23 jaar");
+//        System.out.println("Ik kom uit Groningen");
+//        System.out.println("Ik speel gitaar");
+//        System.out.println("Regel nummer 5!");
+
+        IntroductieClass tijdelijk = new IntroductieClass("Aron");
+        IntroductieClass tijdelijk2 = new IntroductieClass("Herman");
+
+    }
+}
+
