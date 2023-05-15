7a6b80656dce9b36590661d270f2b0e9b106f093
Aron220 <109858751+Aron220@users.noreply.github.com>
2023-05-15 12:11:17 +0200

    2 extra lines
    2 extra lines toegevoegd

--- Intro.java
+++ Intro.java
@@ -5,5 +5,8 @@ public class Intro {
         System.out.println("Ik heet Aron");
         System.out.println("Ik ben 23 jaar");
         System.out.println("Ik kom uit Groningen");
+        System.out.println("Ik speel gitaar");
+        System.out.println("Regel nummer 5!");
+
     }
 }
